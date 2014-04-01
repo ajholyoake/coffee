dc60dcfc6d2a009a7f42a19e0db2099ec308a313
Alex Holyoake <ajholyoake@gmail.com>
2014-04-01 17:09:22 +0100

    New coffees and abstracted out to another text file

--- routes/genlist.js
+++ routes/genlist.js
@@ -1,40 +1,55 @@
 var request = require('request'),
 cheerio = require('cheerio'),
 mongo   = require('mongoskin');
+fs      = require('fs');
 
 
 exports.generate = function(req,res){
   var db = mongo.db('localhost:27017/coffee?auto_reconnect');
   var orderNumber, out = {}, url, coffeelist = [];
   db.collection('coffeelist').find({},{limit:1, sort:[['orderNumber',-1]]}).toArray(function(err,stuff){
-    orderNumber = parseInt(stuff[0].orderNumber);
+    orderNumber = parseInt(stuff[0].orderNumber+1);
     if(req.params.id){ orderNumber = parseInt(req.params.id);}
 
-    out = {};
-    url = "https://www.nespresso.com/uk/en/quickView/";
-    coffeelist = 
-    ["Ristretto",
-      "Arpeggio",
-      "Roma",
-      "Livanto",
-      "Capriccio",
-      "Volluto",
-      "Cosi",
-      "Indriya from India",
-      "Rosabaya de Colombia",
-      "Dulsao do Brasil",
-      "Decaffeinato Intenso",
-      "Decaffeinato Lungo",
-      "Decaffeinato",
-      "Fortissio Lungo",
-      "Vivalto Lungo",
-      "Finezzo Lungo",
-      "Linizio Lungo",
-      "Vanilio",
-      "Caramelito",
-      "Ciocattino"];
-    var ii = 0;
+    var out = {};
+    var url = "https://www.nespresso.com/uk/en/quickView/";
+    var coffeeFile = "coffeelist.txt";
+    fs.readFile(coffeeFile,'utf8',getCoffees);
+   
+   function getCoffees(error,coffeelistcontents){
+    
+    var coffeelist = coffeelistcontents.split(/\n|\r\n/).map(function(x){
+      return x.split('#')[0].trim();
+    }).filter(function(x){ 
+    return /\S/.test(x); });
 
+//    coffeelist = 
+//    [
+//      "Kazaar",
+//      "Dharkan Coffee",
+//      "Ristretto",
+//      "Arpeggio",
+//      "Roma",
+//      "Livanto",
+//      "Capriccio",
+//      "Volluto",
+//      "Cosi",
+//      "Indriya from India",
+//      "Rosabaya de Colombia",
+//      "Dulsao do Brasil",
+//      "Bukeela Ka Ethiopia",
+//      "Decaffeinato Intenso",
+//      "Decaffeinato Lungo",
+//      "Decaffeinato",
+//      "Fortissio Lungo",
+//      "Vivalto Lungo",
+//      "Linizio Lungo",
+//      "Vanilio",
+//      "Caramelito",
+//      "Ciocattino"];
+    var ii = 0;
+    var coffeesdone = 0;
+    
     var requestlistitem = function(ii){
 
 
@@ -42,6 +57,8 @@ exports.generate = function(req,res){
       c = c.replace(/ /g,"-").toLowerCase();
 
       request(url+c,function(error,response,body){
+        if (!error){
+        
         var $ = cheerio.load(body);
         var imgsrc = $('.nes_bloc-degrade-pop img').attr('src');
         var price = $(".nes_list-price").html();
@@ -62,23 +79,25 @@ exports.generate = function(req,res){
           var curl = url + c;
           if(price)
           {out[coffeelist[ii]] = {img: 'https://www.nespresso.com' + imgsrc, price:price, url:curl, types:types, intensity:intensity, longdescription:longdescription, aromaticprofile:aromaticprofile }};
+        }
+            coffeesdone++;
 
-            if (ii === coffeelist.length-1){
+            if(coffeesdone === coffeelist.length-1)
+            {
               var ul = { "orderNumber": orderNumber,
                 "list": out};
                 db.collection('coffeelist').update({"orderNumber":orderNumber},ul,{upsert:true});
                 res.send(JSON.stringify(ul,null,4));
-            }else{
-              ii++;
-              requestlistitem(ii);
             }
 
 
       });
 
     };
+    for(var ii = 0; ii < coffeelist.length; ii++)
+    {
     requestlistitem(ii);
-
-    res.send('Done');
+    }
+  }
   });
 };
